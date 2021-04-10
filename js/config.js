02e50a944b04175fda94b1ee436217ba9a5d3d6c
jcl <juancruz.lepore@gmail.com>
2021-04-10 19:10:58 +0100

    userpool

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_EktffKRP5', // e.g. eu-west-2_EktffKRP5
+        userPoolClientId: '63fm54p46gd046q114mdg31f0k', // e.g. 63fm54p46gd046q114mdg31f0k
+        region: 'eu-west-2' // e.g. eu-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
